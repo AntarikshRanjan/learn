1bfddb7080acc5c011ab9b7ba8dab63f740b4bf5
Antariksh Ranjan <antarikshranjan@gmail.com>
2026-05-10 01:36:09 +0530

    2 and 3

---
+++ 3.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int age;
+    cin >> age;
+
+    if(age >= 18) {
+
+        if(age > 55 && age <= 57) {
+            cout << "retirement soon";
+        }
+        else if(age > 57) {
+            cout << "retirement";
+        }
+        else {
+            cout << "eligible for the role";
+        }
+
+    }
+    else {
+        cout << "not eligible for the role";
+    }
+
+    return 0;
+}
