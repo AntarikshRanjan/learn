886ed2e3a38a343386436aaed435d194888c7f49
Antariksh Ranjan <antarikshranjan@gmail.com>
2026-05-09 22:48:34 +0530

    1st

---
+++ 1.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int age;
+    cin>>age;
+    if(age<18){
+        cout<<"kiddo";
+    }else{
+        cout<<"Bhaiya App";
+    }
+return 0;}
